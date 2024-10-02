1c2eda44828f4ef16da713ab4a65799751c4dccd
8adiq <abdul.latif708@yahoo.com>
2024-10-02 10:10:08 +0000

    basic user model created, migration done

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True,null=True)
+    profile_pic= models.ImageField(upload_to='',blank=True,null=True)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['firstname','lastname','username']
+
+    def __str__(self):
+        return f' Email: {self.email}, Username: {self.username}'
