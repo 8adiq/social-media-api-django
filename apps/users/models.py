14d07e89ad89932d3d462a21942517f8bf24d8c4
8adiq <abdul.latif708@yahoo.com>
2024-10-03 04:43:29 +0000

    regular user creation done

--- apps/users/models.py
+++ apps/users/models.py
@@ -6,11 +6,11 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     bio = models.TextField(blank=True,null=True)
-    profile_pic= models.ImageField(upload_to='',blank=True,null=True)
+    profile_picture= models.ImageField(upload_to='',blank=True,null=True)
 
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['firstname','lastname','username']
+    REQUIRED_FIELDS = ['first_name','last_name','username']
 
     def __str__(self):
         return f' Email: {self.email}, Username: {self.username}'
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets,generics
+from rest_framework.permissions import AllowAny
+from .models import User
+from .serializer import UserSerializer
+
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserRegistration(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
