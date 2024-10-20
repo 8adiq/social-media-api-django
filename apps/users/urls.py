d1bdb30679399ef788321d9d1f1ecc204cab61af
8adiq <abdul.latif708@yahoo.com>
2024-10-20 16:40:08 -0400

    switched to using viewset

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 
 from .views import UserRegistration
 
 
+router = DefaultRouter()
+router.register(r'register',UserRegistration)
 
 urlpatterns = [
-    path('register/',UserRegistration.as_view(),name='user-registration')
+    path('',include(router.urls))
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from .models import User
 from .serializer import UserSerializer
-from rest_framework import generics
+from rest_framework import generics,viewsets
 from rest_framework.permissions import AllowAny
 # Create your views here.
 
 
-class UserRegistration(generics.CreateAPIView):
+class UserRegistration(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
