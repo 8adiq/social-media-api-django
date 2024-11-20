890d6ba3308bdac501a90ff8ecde32c6e73c3ee3
8adiq <abdul.latif708@yahoo.com>
2024-11-20 16:24:49 -0500

    fixing error

--- config/deployment.py
+++ config/deployment.py
@@ -3,8 +3,8 @@ from .settings import *
 from .settings import BASE_DIR
 
 
-ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME','social-media-api-bsfmbnbbh4g6hvfy.southafricanorth-01.azurewebsites.net')]
-CSRF_TRUSTED_ORIGINS = ['https:/'+ os.environ.get('WEBSITE_HOSTNAME','social-media-api-bsfmbnbbh4g6hvfy.southafricanorth-01.azurewebsites.net')]
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https:/'+ os.environ['WEBSITE_HOSTNAME']]
 DEBUG=False
 SECRET_KEY = os.environ['SECRET']
 
