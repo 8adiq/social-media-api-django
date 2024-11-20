8bc7913d06a843aa92ef27b86d36c4b546254c4b
8adiq <abdul.latif708@yahoo.com>
2024-11-20 15:53:35 -0500

    fixed errors

--- config/deployment.py
+++ config/deployment.py
@@ -3,8 +3,8 @@ from .settings import *
 from .settings import BASE_DIR
 
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOST']]
-CSRF_TRUSTED_ORIGINS = ['https:/'+os.environ['WEBSITE_HOST']]
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https:/'+os.environ['WEBSITE_HOSTNAME']]
 DEBUG=False
 SECRET_KEY = os.environ['SECRET']
 
